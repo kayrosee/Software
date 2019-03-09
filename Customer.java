977b9d52c675048ce350b2079daa3ac07d6acab9
kayrosee <48164847+kayrosee@users.noreply.github.com>
2019-03-09 15:00:56 -0800

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,15 +2,11 @@ package Software;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import lab5.TodoEntry;
 
 /**
  * Servlet implementation class Customer
@@ -31,8 +27,7 @@ public class Customer extends HttpServlet {
 		out.println("<head>");
 		out.println("	<meta charset=\"UTF-8\">");
 		out.println("	<title>Customer Resturant</title>");
-		out.println(
-				"	<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">");
+		out.println("	<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">");
 		out.println("</head>");
 		out.println("<body>");
 		out.println("<div class=\"container\">");
@@ -42,9 +37,10 @@ public class Customer extends HttpServlet {
 		out.println("	<form action=\"CarlsJr\" method=\"post\">");
 		out.println("	<input class=\"btn btn-primary\" type=\"submit\" name=\"submitBtn\" value=\"Carl's Jr\"></form><br>");
 
-		//El Pollo Loco
+		// El Pollo Loco
 		out.println("	<form action=\"ElPolloLoco\" method=\"post\">");
-		out.println("	<input class=\"btn btn-primary\" type=\"submit\" name=\"submitBtn\" value=\"El Pollo Loco\"></form><br>");
+		out.println(
+				"	<input class=\"btn btn-primary\" type=\"submit\" name=\"submitBtn\" value=\"El Pollo Loco\"></form><br>");
 
 		// Johny's Kitchen
 		out.println("	<form action=\"JohnysKitchen\" method=\"post\">");
@@ -59,25 +55,7 @@ public class Customer extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		// Check to see if the Add New Entry form was submitted
-		// We'll do so by validating the inputs
-
-		String todo = request.getParameter("todo");
-
-		if (todo != null && todo.trim().length() > 0) {
-
-			ArrayList<TodoEntry> todoEntries = (ArrayList<TodoEntry>) getServletContext().getAttribute("todoEntries");
-
-			// Add a new Entry to the guest book using the name and message that were
-			// submitted
-
-			todoEntries.add(new TodoEntry(todo));
-
-			// Send the User (Client) back to the GuestBook page
-			response.sendRedirect("Todo");
-		} else
-			doGet(request, response);
+		doGet(request, response);
 
 	}
 
